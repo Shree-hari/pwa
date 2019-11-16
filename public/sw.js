c19a2f361ed697387cfd4c78b355e80bfe1f8b7d
Shree-hari <harshmacc@gmail.com>
2019-11-16 17:43:18 +0530

    installing and activating the service worker

---
+++ public/sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install' , function(event){
+	console.log('[Service Worker] Installing service worker ....', event);
+});
+self.addEventListener('activate' , function(event){
+	console.log('[Service Worker] Activating service worker ....', event);
+	return self.clients.claim();
+});
