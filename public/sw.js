931d1fec64135d84762580020b52b1e6a455a3da
Shree-hari <harshmacc@gmail.com>
2019-11-16 19:36:28 +0530

    setting when to trigger the add to home screen prompt
    basically we set that + sign as a trigger for the user to add the pwa to the home screen

--- public/sw.js
+++ public/sw.js
@@ -5,3 +5,7 @@ self.addEventListener('activate' , function(event){
 	console.log('[Service Worker] Activating service worker ....', event);
 	return self.clients.claim();
 });
+self.addEventListener('fetch' , function(event){
+	console.log('[Service Worker] Fetching service worker ....', event);
+	event.respondWith(fetch(event.request));
+});
